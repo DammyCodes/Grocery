10364ea723cb4c176903216cd13b75ded891a396
Damilola Okunlola <okunlolatawakalitu61@gmail.com>
2024-03-01 22:41:09 +0100

    save

---
+++ index.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('form')
+let inputField = document.getElementById('input-field')
+const clearItemsBtn = document.getElementById('clear')
+
+let Delete = e => {
+    e.parentElement.parentElement.remove()
+}
+
+let edit = e => {
+    inputField.value = e.parentElement.previousElementSibling.textContent
+
+    e.parentElement.parentElement.remove()
+}
+
+let renderListItems = e => {
+    e.preventDefault()
+
+    if (inputField.value) {
+        document.getElementById('list').innerHTML +=  `
+    
+            <div class="item">
+                <p>${inputField.value}</p>
+                <span>
+                    <i class="fa-solid fa-pen-to-square" onclick="edit(this)"></i>
+                    <i class="fa-solid fa-trash" onclick="Delete(this)"></i>
+                </span>
+            </div>
+         
+        `
+    }
+    inputField.value = ''
+   
+}
+
+form.addEventListener('submit', renderListItems)
+
+clearItemsBtn.addEventListener('click', () => document.getElementById('list').innerHTML = '' )
+
+
